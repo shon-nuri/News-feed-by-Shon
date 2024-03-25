96a2068bb61c502130481ed5f9d26f77cc40cfe5
shon_nuri <nadyrkhans@gmail.com>
2024-03-25 08:44:40 +0500

    fullsize image added

---
+++ newspaper_project/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+    path('articles/', include('articles.urls')),
+    path('', include('pages.urls')),
+]
